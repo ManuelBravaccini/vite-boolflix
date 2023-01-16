8c28dcabfc9b7fa036e7b7101cedd7b00bad2a82
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-01-16 12:36:05 +0100

    Linked app components to store.js and completed scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'AppHeader',
+
+    data() {
+        return {
+            store
+        }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    },
+}
+
+</script>
+
+<template>
+    <header></header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            store
+        }
+    },
+}
+
+</script>
+
+<template>
+    <main></main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
